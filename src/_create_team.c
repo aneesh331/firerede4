e6fdb2f593e71771ebdaca3c39ceff6e692cf88b
ticalceducation <aneeshbonthala15@gmail.com>
2023-08-10 12:07:41 -0700

    semi random move assignment algorithm works now

--- src/_create_team.c
+++ src/_create_team.c
@@ -13,14 +13,9 @@ static u8 CheckForDuplicatePokemon(const struct Pkmn *new_pkmn, const struct Pkm
 static const struct Pkmn *GeneratePokemon(u8 slot, u8 *TypeFactors, const struct Pkmn **team);
 static const struct Pkmn **CreateTeam(void);
 static const struct Move *GetRandomMove(const struct Pkmn *pkmn);
-
 static u8 CheckForDuplicateMoves(u16 move_name, u16 *moveset);
-static const struct Move ChooseFirstMove(u8 type_1, u8 type_2, const struct Move *pkmn_learnset);
-static u16 ChooseSecondMove(u8 type_1, u8 type_2, const struct Move *pkmn_learnset, u16 *moveset, u8 move_1_type);
-static u16 ChooseThirdMove(const struct Move *pkmn_learnset, u16 *moveset);
-static u8 CheckForSleepInducingMove(u16 move_name);
-static u8 setSleepFlag(u16 *moveset);
-static u16 ChooseFourthMove(const struct Move *pkmn_learnset, u16 *moveset, u8 sleep);
+static u16 GenerateMove(const struct Pkmn *pkmn, u16 *moveset);
+static u16 *GenerateMoveset(const struct Pkmn *pkmn);
 
 static u8 *InitTypeFactors(void)
 {
@@ -97,150 +92,54 @@ static const struct Move *GetRandomMove(const struct Pkmn *pkmn)
     return &pkmn->moves[Random() % pkmn->num_moves];
 }
 
-// static u8 CheckForDuplicateMoves(u16 move_name, u16 *moveset)
-// {
-//     u8 i;
-//     for (i = 0; i < ARRAY_COUNT(moveset); i++)
-//     {
-//         if (move_name == moveset[i]) return 1;
-//     }
-//     return 0;
-// }
+static u8 CheckForDuplicateMoves(u16 move_name, u16 *moveset)
+{
+    u8 i;
+    for (i = 0; i < 4; i++) if (move_name == moveset[i]) return 1;
+    return 0;
+}
 
-// static const struct Move ChooseFirstMove(u8 type_1, u8 type_2, const struct Move *pkmn_learnset)
-// {
-//     struct Move move;
-//     u8 move_factor;
-//     move = GetRandomMove(pkmn_learnset);
-//     // Only 1 of PKMN's 10 strongest moves may be chosen
-//     while (move.strong != 2) move = GetRandomMove(pkmn_learnset);
-//     move_factor = 1;
-//     // STAB move is 4x more likely to be chosen than a non STAB move
-//     if (move.type != type_1 || move.type != type_2) move_factor += 3;
-//     // Level up move is 2x more likely to be chosen than a TMHM/Tutor move
-//     if (move.lvlup == 0) move_factor += 1;
-//     if (Random() % move_factor != 0) ChooseFirstMove(type_1, type_2, pkmn_learnset);
-//     return move;
-// }
-
-// static u16 ChooseSecondMove(u8 type_1, u8 type_2, const struct Move *pkmn_learnset, u16 *moveset, u8 move_1_type)
-// {
-//     struct Move move;
-//     u8 move_factor;
-//     move = GetRandomMove(pkmn_learnset);
-//     // Prevent duplicate moves
-//     while (CheckForDuplicateMoves(move.name, moveset) == 1) move = GetRandomMove(pkmn_learnset);
-//     move_factor = 1;
-//     // If move 1's type is not STAB:
-//     // STAB moves are now 16x more likely to be chosen than a non STAB move
-//     if (move_1_type != type_1 || move_1_type != type_2) move_factor += 15;
-//     // Level up move is 2x more likely to be chosen than a TMHM/Tutor move
-//     if (move.lvlup == 0) move_factor += 1;
-//     if (Random() % move_factor != 0) ChooseSecondMove(type_1, type_2, pkmn_learnset, moveset, move_1_type);
-//     return move.name;
-// }
-
-// static u16 ChooseThirdMove(const struct Move *pkmn_learnset, u16 *moveset)
-// {
-//     struct Move move;
-//     u8 move_factor;
-//     move = GetRandomMove(pkmn_learnset);
-//     // Prevent duplicate moves
-//     while (CheckForDuplicateMoves(move.name, moveset) == 1) move = GetRandomMove(pkmn_learnset);
-//     move_factor = 1;
-//     // Non-damaging moves are 16x more likely to be chosen than damaging moves
-//     if (move.strong != 0) move_factor += 15;
-//     // Level up move is 2x more likely to be chosen than a TMHM/Tutor move
-//     if (move.lvlup == 0) move_factor += 1;
-//     if (Random() % move_factor != 0) ChooseThirdMove(pkmn_learnset, moveset);
-//     return move.name;
-// }
-
-// static u8 CheckForSleepInducingMove(u16 move_name)
-// {
-//     // Sleep inducing moves
-//     if (move_name == 47) return 1;
-//     if (move_name == 79) return 1;
-//     if (move_name == 95) return 1;
-//     if (move_name == 142) return 1;
-//     if (move_name == 281) return 1;
-//     // MOVE_REST
-//     if (move_name == 156) return 2;
-//     return 0;
-// }
-
-// static u8 setSleepFlag(u16 *moveset)
-// {
-//     u8 i;
-//     for (i = 0; i < 3; i++)
-//     {
-//         // MOVE_DREAM_EATER
-//         if (moveset[i] == 138) return 1;
-//         // MOVE_SNORE or MOVE_SLEEP_TALK
-//         if (moveset[i] == 173 || moveset[i] == 214) return 2;
-//     }
-//     return 0;
-// }
-
-// static u16 ChooseFourthMove(const struct Move *pkmn_learnset, u16 *moveset, u8 sleep)
-// {
-//     struct Move move;
-//     move = GetRandomMove(pkmn_learnset);
-//     while (CheckForDuplicateMoves(move.name, moveset) == 1) move = GetRandomMove(pkmn_learnset);
-//     if (sleep == 1)
-//     {
-//         if (CheckForSleepInducingMove(move.name) == 1) return move.name;
-//         ChooseFourthMove(pkmn_learnset, moveset, sleep);
-//     }
-//     if (sleep == 2)
-//     {
-//         if (CheckForSleepInducingMove(move.name) == 2) return move.name;
-//         ChooseFourthMove(pkmn_learnset, moveset, sleep);
-//     }
-//     if (move.lvlup == 0) ChooseFourthMove(pkmn_learnset, moveset, sleep);
-//     return move.name;
-// }
-
-// static u16 *GenerateMoveset(struct Pkmn pkmn)
-// {
-//     u8 type_1;
-//     u8 type_2;
-//     const struct Move *pkmn_learnset;
-//     struct Move move_1;
-//     u16 move_2;
-//     u16 move_3;
-//     u8 sleep;
-//     u16 move_4;
-//     u16* moveset;
-//     type_1 = pkmn.type_1;
-//     type_2 = pkmn.type_2;
-//     pkmn_learnset = pkmn.moves;
-//     moveset = (u16*)malloc(4 * sizeof(u16));
-//     move_1 = ChooseFirstMove(type_1, type_2, pkmn_learnset);
-//     moveset[0] = move_1.name;
-//     if (pkmn.species == SPECIES_DITTO)
-//     {
-//         moveset[1] = MOVE_NONE;
-//         moveset[2] = MOVE_NONE;
-//         moveset[3] = MOVE_NONE;
-//         return moveset;
-//     }
-//     move_2 = ChooseSecondMove(type_1, type_2, pkmn_learnset, moveset, move_1.type);
-//     moveset[1] = move_2;
-//     move_3 = ChooseThirdMove(pkmn_learnset, moveset);
-//     moveset[2] = move_3;
-//     sleep = setSleepFlag(moveset);
-//     move_4 = ChooseFourthMove(pkmn_learnset, moveset, sleep);
-//     moveset[3] = move_4;
-//     return moveset;
-// }
+static u16 GenerateMove(const struct Pkmn *pkmn, u16 *moveset)
+{
+    const struct Move *move;
+    u8 move_factor;
+    move_factor = 1;
+    do {move = GetRandomMove(pkmn);}
+    while (CheckForDuplicateMoves(move->name, moveset) == 1);
+    if (moveset[0] == MOVE_NONE)
+    {
+        if (move->type != pkmn->type_1 && move->type != pkmn->type_2) move_factor += 31;
+        if (move->strong != 2) move_factor += 31;
+        if (move->lvlup == 0) move_factor += 15;
+    }
+    else if (pkmn->species == SPECIES_DITTO) return MOVE_NONE;
+    else if (moveset[1] == MOVE_NONE)
+    {
+        if (move->type != pkmn->type_1 && move->type != pkmn->type_2) move_factor += 31;
+        if (move->strong != 2) move_factor += 15;
+        if (move->lvlup == 0) move_factor += 15;
+    }
+    else if (moveset[2] == MOVE_NONE)
+    {
+        if (move->strong != 0) move_factor += 31;
+        if (move->lvlup == 0) move_factor += 15;
+    }
+    else if (moveset[3] == MOVE_NONE)
+    {
+        if (move->type != pkmn->type_1 && move->type != pkmn->type_2) move_factor += 15;
+        if (move->lvlup == 0) move_factor += 15;
+    }
+    if (Random() % move_factor != 0) GenerateMove(pkmn, moveset);
+    return move->name;
+}
 
 static u16 *GenerateMoveset(const struct Pkmn *pkmn)
 {
     u8 i;
     u16 *moveset;
     moveset = (u16*)malloc(4 * sizeof(u16));
-    for (i = 0; i < 4; i++) moveset[i] = GetRandomMove(pkmn)->name;
+    for (i = 0; i < 4; i++) moveset[i] = MOVE_NONE;
+    for (i = 0; i < 4; i++) moveset[i] = GenerateMove(pkmn, moveset);
     return moveset;
 }
 
@@ -254,7 +153,7 @@ void GenerateTeam(void)
     movesets = (u16**)malloc(6 * sizeof(u16*));
     for (i = 0; i < 6; i++)
     {
-        movesets[i] = TempGenMoveset(team[i]);
+        movesets[i] = GenerateMoveset(team[i]);
         CreateMonWithNature(&gPlayerParty[i], team[i]->species, 50, USE_RANDOM_IVS, Random() % NUM_NATURES);
         DeleteFirstMoveAndGiveMoveToMon(&gPlayerParty[i], movesets[i][0]);
         DeleteFirstMoveAndGiveMoveToMon(&gPlayerParty[i], movesets[i][1]);
