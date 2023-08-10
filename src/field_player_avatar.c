c11426e296ecd75fdf29985404954900454cd8fd
ticalceducation <aneeshbonthala15@gmail.com>
2023-08-10 14:58:26 -0700

    Changed some scripts and allowed running

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -513,8 +513,7 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
         return;
     }
 
-    if ((heldKeys & B_BUTTON) && FlagGet(FLAG_SYS_B_DASH)
-        && !IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior))
+    if ((heldKeys & B_BUTTON))
     {
         if (PlayerIsMovingOnRockStairs(direction))
             PlayerRunSlow(direction);
